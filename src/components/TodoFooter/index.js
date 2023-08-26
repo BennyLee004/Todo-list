135b2e2207f6983b64d8785d6e21647d1a045a9d
Thien <toihctatca2022@gmail.com>
2023-08-26 09:48:49 +0700

    Complete UI

---
+++ src/components/TodoFooter/index.js
@@ -0,0 +1,9 @@
+
+export default function TodoFooter () {
+    return (
+        <div className="todo-footer">
+            <p>5 task left!</p>
+            <p>MindX todolist</p>
+        </div>
+    )
+}
--- src/components/TodoHeader/index.js
+++ src/components/TodoHeader/index.js
@@ -1,1 +1,14 @@
-console.log('anything')
+
+import "./style.css"
+
+export default function TodoHeader  () {
+    return (
+        <div className="todo-header">
+            <input
+                type="text"
+                placeholder="Enter your task here ..."
+            />
+            <hr/>
+        </div>
+    )
+}
